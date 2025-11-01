03dc74b9f8185083e0a6ddde3af2f0c83512a834
bijita-syanz <164947014+bijita-syanz@users.noreply.github.com>
2025-11-01 21:14:21 +0100

    Allow terminal area to be editable
    chnage readonly to read and write

--- app.cpp
+++ app.cpp
@@ -167,7 +167,7 @@ int main(int argc, char *argv[]) {
     // Create UI elements
     QTextEdit *codeArea = new QTextEdit();
     QTextEdit *terminalArea = new QTextEdit();
-    terminalArea->setReadOnly(true);
+    terminalArea->setReadOnly(false);
 
     QPushButton *saveButton = new QPushButton("Save Script");
     QPushButton *runButton = new QPushButton("Run Manim");
